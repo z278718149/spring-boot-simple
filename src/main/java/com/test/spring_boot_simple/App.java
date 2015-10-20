5dabd706fa0654bcc8d2a43098972f774125c5c8
z278718149 <278718149@qq.com>
2015-10-20 10:22:38 +0800

    test

---
+++ src/main/java/com/test/spring_boot_simple/App.java
@@ -0,0 +1,13 @@
+package com.test.spring_boot_simple;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
